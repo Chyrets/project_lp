81f218d837b829436d0145b8e3243b7cabaf98f1
Chyrets <uladzislawchyrets@gmail.com>
2021-12-17 18:46:39 +0300

    Добавил тест для метода __str__ модели Profile

--- profiles/tests/test_models.py
+++ profiles/tests/test_models.py
@@ -10,9 +10,14 @@ class ProfileModelTests(TestCase):
     def setUp(self) -> None:
         self.user = User.objects.create(username='test_user', password='password')
 
+    def test_object_name_is_user_name(self):
+        """Проверка отображения информации класса для пользователя"""
+        profile = Profile.objects.get(user=self.user)
+        expected_object_name = f'{profile.name}'
+        self.assertEqual(expected_object_name, str(profile))
+
     def test_create_user_profile_for_new_user(self):
         """Проверка создания профиля для новых пользователей"""
         profile = Profile.objects.get(user=self.user)
         self.assertEqual(profile.name, self.user.username)
         self.assertEqual(profile.user, self.user)
-
