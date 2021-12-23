9fe03b4019d4f07df96cdb0617723ecec7bb205d
Chyrets <uladzislawchyrets@gmail.com>
2021-12-23 11:02:06 +0300

    Добавил модель тега и подключил ее к админке

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag
+
+
+class TagAdmin(admin.ModelAdmin):
+    """Настройка отображения модели Tag в админке"""
+    list_display = ('id', 'title', 'author')
+    list_display_links = ('id', 'title')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Tag, TagAdmin)
--- content/models.py
+++ content/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+from profiles.models import Profile
+
+
+class Tag(models.Model):
+    """Модель тегов для поста"""
+    title = models.CharField("Название", max_length=50)
+    slug = models.SlugField("URL", null=False, unique=True)
+    author = models.ForeignKey(
+        Profile,
+        verbose_name="Автор",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
