bde304ab4d911883db1edca65c4b83a7bc8a4ad1
Chyrets <uladzislawchyrets@gmail.com>
2021-12-23 12:55:57 +0300

    Написал тест для модели Profile

--- content/tests/test_models.py
+++ content/tests/test_models.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.utils.text import slugify
 
 from profiles.models import Profile
-from ..models import Tag
+from ..models import Tag, Post
 
 
 class TagModelTests(TestCase):
@@ -25,3 +25,18 @@ class TagModelTests(TestCase):
         tag = Tag.objects.first()
         expected_tag_slug = slugify(tag.title)
         self.assertEqual(expected_tag_slug, tag.slug)
+
+
+class PostModelTests(TestCase):
+    """Тесты для модели Post"""
+
+    def setUp(self) -> None:
+        User.objects.create_user(username='test_user', password='password')
+        profile = Profile.objects.first()
+        Post.objects.create(title="post1", caption='About post1', author=profile)
+
+    def test_object_name_is_post_title_by_author(self):
+        """Проверка того, что имя объекта является именем поста и его автора"""
+        post = Post.objects.first()
+        expected_obj_name = f'{post.title} by {post.author}'
+        self.assertEqual(expected_obj_name, str(post))
