117d09dc7c288e455f8d4f1b64864e1ead161326
Chyrets <uladzislawchyrets@gmail.com>
2021-12-24 18:07:02 +0300

    Написал тесты для формы добавления поста

--- content/urls.py
+++ content/urls.py
@@ -6,5 +6,5 @@ app_name = 'content'
 urlpatterns = [
     path('posts/<slug:profile_slug>/', views.ProfilePostsView.as_view(), name='profile_posts_list'),
     path('post-detail/<int:post_id>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('add-post/', views.AddPostView.as_view(), name='add-post')
+    path('add-post/', views.AddPostView.as_view(), name='add_post')
 ]
