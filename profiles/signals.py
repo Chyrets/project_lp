93972a3287cadf8b70baada14aab15f3f248590c
Chyrets <uladzislawchyrets@gmail.com>
2022-01-05 12:01:38 +0300

    Добавил поле 'используется' для модели Profile

--- profiles/signals.py
+++ profiles/signals.py
@@ -9,4 +9,11 @@ from .models import Profile
 def create_profile_for_new_user(sender, instance, created, **kwargs):
     """Создание профиля для нового пользователя"""
     if created:
-        Profile.objects.create(name=instance.username, user=instance)
+        Profile.objects.create(name=instance.username, user=instance, used=True)
+
+
+@receiver(post_save, sender=Profile)
+def change_used_profile(sender, instance, **kwargs):
+    """Изменение используемого аккаунта"""
+    Profile.objects.filter(used=True, user=instance.user).update(used=False)
+    Profile.objects.filter(pk=instance.pk).update(used=True)
