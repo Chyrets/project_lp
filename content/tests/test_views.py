02513c52db1a8104211353e509a8b517c81be511
Chyrets <uladzislawchyrets@gmail.com>
2022-01-06 18:22:42 +0300

    Написал тесты для функционала добавления лайков

--- content/tests/test_views.py
+++ content/tests/test_views.py
@@ -1,8 +1,10 @@
+from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 
-from content.models import Post
+from content.models import Post, PostReaction
+from content.services.view_services import add_remove_reaction
 from profiles.models import Profile
 
 
@@ -202,3 +204,57 @@ class EditPostViewTest(TestCase):
         self.assertEqual(post.caption, 'About test post1 dt')
         self.assertEqual(tags, '#test_tag1')
         self.assertEqual(post.archived, True)
+
+
+class PostReactionViewTest(TestCase):
+    """Тесты для функционала добавления реакции"""
+
+    def setUp(self) -> None:
+        test_user1 = User.objects.create_user(username='test_user1', password='password')
+        test_user1.save()
+        self.profile = Profile.objects.first()
+        self.test_post = Post.objects.create(
+            title='test post1',
+            caption='About test post1 by test_user1',
+            author=self.profile
+        )
+        ct_post = ContentType.objects.get_for_model(Post)
+        self.valid_data = {
+            'profile': self.profile,
+            'content_type': ct_post,
+            'object_id': self.test_post.pk,
+            'reaction': 1
+        }
+
+    def test_add_like_reaction(self):
+        """Проверка добавления лайка к посту"""
+        self.client.login(username='test_user1', password='password')
+        response = self.client.get(
+            reverse('content:post_reaction', kwargs={'post_id': self.test_post.pk, 'reaction': 1}))
+        likes = PostReaction.objects.filter().count()
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(likes, 1)
+
+    def test_services_add_remove_reaction(self):
+        """Проверка функции add_remove_reaction"""
+        # Добавляем лайк
+        add_remove_reaction(**self.valid_data)
+        likes = PostReaction.objects.filter().count()
+        self.assertEqual(likes, 1)
+
+        # Меняем лайк на дизлайк
+        self.valid_data['reaction'] = 2
+        add_remove_reaction(**self.valid_data)
+        dislike = PostReaction.objects.filter().count()
+        self.assertEqual(dislike, 1)
+
+        # Убираем дизлайк
+        add_remove_reaction(**self.valid_data)
+        dislike = PostReaction.objects.filter().count()
+        self.assertEqual(dislike, 0)
+
+        # Добавляем дизлайк
+        add_remove_reaction(**self.valid_data)
+        dislike = PostReaction.objects.filter().count()
+        self.assertEqual(dislike, 1)
