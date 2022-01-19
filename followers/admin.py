6d7b7d9161cf668bd93b1c5059b19ef96f6a70fc
Chyrets <uladzislawchyrets@gmail.com>
2022-01-19 19:07:56 +0300

    Добавил модел подписчиков и подключил её к админке

--- followers/admin.py
+++ followers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Follower
+
+
+@admin.register(Follower)
+class FollowerAdmin(admin.ModelAdmin):
+    """Настройка отображения модели Follower в админке"""
+    list_display = ('id', 'recipient', 'sender')
+    list_display_links = ('recipient', 'sender')
--- followers/models.py
+++ followers/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Follower(models.Model):
+    """Модель подписчиков"""
+    recipient = models.ForeignKey(
+        User,
+        verbose_name="Адресат",
+        on_delete=models.CASCADE,
+        related_name='recipients',
+    )
+    sender = models.ForeignKey(
+        User,
+        verbose_name="Адресант",
+        on_delete=models.CASCADE,
+        related_name='senders'
+    )
+
+    def __str__(self):
+        return f'{self.recipient} followed by {self.sender}'
+
+    class Meta:
+        verbose_name = "Подписчик"
+        verbose_name_plural = "подписчики"
